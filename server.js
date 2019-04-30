53fc3d2d9442afdd58c386a7da65bfe28193cad4
Somoye <somoye.ayotunde@gmail.com>
2019-04-30 15:09:47 -0800

    fix : Call toTimeString method

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 // logger middleware
 app.use((req, res, next) => {
-  console.log(`[${new Date().toTimeString}]: ${req.method} ${req.url}`);
+  console.log(`[${new Date().toTimeString()}]: ${req.method} ${req.url}`);
   next();
 });
 
